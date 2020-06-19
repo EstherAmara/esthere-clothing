d203e865ef2a450a59fa52134187a6f27fb1a464
esthereamara <esthere.amara@gmail.com>
2020-06-19 12:39:18 +0100

    sign in form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInUp from './pages/sign-in-up/sign-in-up.component';
 import Header from './components/header/header.component';
 import {Route, Switch} from 'react-router-dom';
 
@@ -12,7 +13,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInUp} />
       </Switch>
     </div>
   );
