3d83cf00fbad5fe0e3b51cdd2304bce2c3ea502b
esthereamara <esthere.amara@gmail.com>
2020-06-25 03:32:41 +0100

    implemented sign in with email and password

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInUp from './pages/sign-in-up/sign-in-up.component';
 import Header from './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -21,10 +21,24 @@ class App extends React.Component {
   unsuscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsuscribeFromAuth = auth.onAuthStateChanged(user=> {
-      this.setState({currentUser: user});
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async userAuth=> {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }) 
 
-      console.log(user);
+          console.log(this.state)
+        })
+      } else {
+        this.setState({currentUser: userAuth}, console.log(this.state));
+      }
+      
     })
   }
 
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,8 +18,8 @@ const Header = ({currentUser}) => (
                 CONTACT
             </Link>
             {currentUser 
-            ? (<div className="option" onClick={() => {auth.signOut()}}> SIGN IN </div>) 
-            : (<Link className="option" to="/signin"> SIGN OUT </Link>)}
+            ? (<div className="option" onClick={() => {auth.signOut()}}> SIGN OUT </div>) 
+            : (<Link className="option" to="/signin"> SIGN IN </Link>)}
         </div>
     </div>
 )
